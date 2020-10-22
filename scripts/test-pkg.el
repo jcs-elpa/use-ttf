e430695957011aeff51fe7e7ed1a154226f205f5
Jen-Chieh <jcs090218@gmail.com>
2020-10-22 13:11:36 +0800

    Split section.

--- scripts/test-pkg.el
+++ scripts/test-pkg.el
@@ -1,9 +1,16 @@
+;;
+;; (@* "Prepare" )
+;;
 
 (load-file (expand-file-name "./scripts/pkg-prepare.el"))
 
 (jcs-ensure-package-installed '(s) t)
 
 (load-file (expand-file-name "./use-ttf.el"))
+
+;;
+;; (@* "Test" )
+;;
 
 (setq use-ttf-default-ttf-fonts
       '("../fonts/clacon.ttf"

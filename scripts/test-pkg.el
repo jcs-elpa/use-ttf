717f1301d54da69da64be5bac4235f94a788d3c6
Jen-Chieh <jcs090218@gmail.com>
2020-10-22 12:25:29 +0800

    Make load file.

--- scripts/test-pkg.el
+++ scripts/test-pkg.el
@@ -1,11 +1,11 @@
 
 (message "------> %s" (expand-file-name "../"))
 
-(add-to-list 'load-path (expand-file-name "../"))
-
-(use-ttf-set-default-font)
+(load-file (expand-file-name "../use-ttf.el"))
 
 (setq use-ttf-default-ttf-fonts
       '("../fonts/clacon.ttf"
         "../fonts/UbuntuMono-R.ttf"))
 (setq use-ttf-default-ttf-font-name "Ubuntu Mono")
+
+(use-ttf-set-default-font)

c6207a97459008c20fe07eb53cebd4be0c787956
Jen-Chieh <jcs090218@gmail.com>
2020-10-22 12:42:28 +0800

    Try prepare.

--- scripts/test-pkg.el
+++ scripts/test-pkg.el
@@ -1,11 +1,13 @@
 
-(message "------> %s" (expand-file-name "../"))
+(load-file (expand-file-name "./scripts/pkg-prepare.el"))
+
+(jcs-ensure-package-installed '(s))
 
 (load-file (expand-file-name "./use-ttf.el"))
 
 (setq use-ttf-default-ttf-fonts
       '("../fonts/clacon.ttf"
-        "../fonts/UbuntuMono-R.ttf"))
-(setq use-ttf-default-ttf-font-name "Ubuntu Mono")
+        "../fonts/UbuntuMono-R.ttf")
+      use-ttf-default-ttf-font-name "Ubuntu Mono")
 
 (use-ttf-set-default-font)
